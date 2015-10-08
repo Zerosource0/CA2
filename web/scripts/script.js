e80882e9ebcd647359a17534b1e1442f1e52fde0
Stargarth <adamek1221@gmail.com>
2015-10-08 11:11:52 +0200

    Website 0.1
    Basic website

---
+++ web/scripts/script.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function getPeopleFromHobby()
+{
+    alert("Hobby works");
+}
+
+function getPeopleFromCity()
+{
+    alert("City works");
+}
+
+function getCountHobbyName()
+{
+   alert("countHobbies works") ;
+}
+
+function getZipCodes()
+{
+    alert("zips are working");
+}
+
+function getCompaniesWithMoreThanEmployees()
+{
+    alert("more than is working");
+}
